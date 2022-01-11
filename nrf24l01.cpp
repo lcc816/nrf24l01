d5a92c3b61678f5aa3d2a3cab79eea5e683be0a0
Changchun Lee <lichangchun816@163.com>
2022-01-12 00:26:22 +0800

    Fist commit

---
+++ nrf24l01.cpp
@@ -0,0 +1,305 @@
+/*******************************************************************************
+* @file     nrf24l01.cpp
+* @author   lcc
+* @version  1.0
+* @date     10-Jan-2022
+* @brief    Implementation of the NRF24L01 Class
+*******************************************************************************/
+
+#include "nrf24l01.h"
+
+Nrf24l01::Nrf24l01(uint8_t ce_pin, uint8_t csn_pin, SPIClass *spi_obj)
+{
+    ce_pin_ = ce_pin;
+    csn_pin_ = csn_pin;
+    spi = spi_obj;
+    channel = 1;
+    length_mode = LENGTH_DYN;
+    payload_len = 16;
+    repeat_cnt = 10;
+}
+
+void Nrf24l01::init()
+{
+    pinMode(ce_pin_, OUTPUT);
+    pinMode(csn_pin_, OUTPUT);
+
+    ce_low();
+    csn_high();
+    spi->beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+    spi->begin();
+}
+
+void Nrf24l01::config()
+{
+    if (length_mode == LENGTH_DYN)
+    {
+        // Enable dynamic payload length data pipe 0
+        spi_write_reg(DYNPD, DPL_P0);
+        // Enables Dynamic Payload Length, Payload with ACK,
+        // and the W_TX_PAYLOAD_NOACK command
+        spi_write_reg(FEATRUE, EN_DPL | EN_ACK_PAY | EN_DYN_ACK);
+    }
+    else
+    {
+        spi_write_reg(RX_PW_P0, payload_len);
+        spi_write_reg(RX_PW_P1, payload_len);
+    }
+    spi_write_reg(CONFIG, EN_CRC | PWR_UP);
+    spi_write_reg(EN_AA, ENAA_P0);
+    spi_write_reg(EN_RXADDR, ERX_P0);
+    spi_write_reg(SETUP_AW, AW_5BYTES);
+    spi_write_reg(SETUP_RETR, ARD_4000US |
+                  (repeat_cnt & 0x0F));
+    spi_write_reg(RF_CH, channel);
+    // Set RF Data Rate to 250kbps
+    // Set RF output power to 0dBm
+    spi_write_reg(RF_SETUP, RF_DR_LOW | RF_PWR);
+}
+
+// Only RX_ADDR_P0 and RX_ADDR_P1 have a width of 5 bytes
+// RX_ADDR_P2~5 only have LSB
+// and the MSBytes of RX_ADDR_P2~5 are equal to RX_ADDR_P1[39:8]
+void Nrf24l01::set_rx_addr(uint8_t pipe, uint8_t *addr, uint8_t len)
+{
+    len = (len > 5) ? 5 : len;
+    pipe = (pipe > 5) ? 5 : pipe;
+
+    spi_write_buffer(RX_ADDR_P0 + pipe, addr, 5);
+}
+
+void Nrf24l01::set_tx_addr(uint8_t *addr, uint8_t len)
+{
+    len = (len > 5) ? 5 : len;
+    spi_write_buffer(TX_ADDR, addr, len);
+    /*
+     * RX_ADDR_P0 must be set to the sending addr for auto ack to work.
+     */
+    spi_write_buffer(RX_ADDR_P0, addr, len);
+}
+
+bool Nrf24l01::is_available()
+{
+    uint8_t i;
+    uint8_t tx_addr[5] = {'A', 'B', 'C', 'D', 'E'};
+    uint8_t read_buf[5] = {0};
+    spi_write_buffer(TX_ADDR, tx_addr, 5);
+    spi_read_buffer(TX_ADDR, read_buf, 5);
+
+    for (i = 0; i < 5; i++)
+    {
+        if (tx_addr[i] != read_buf[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void Nrf24l01::ce_high()
+{
+    digitalWrite(ce_pin_, HIGH);
+}
+
+void Nrf24l01::ce_low()
+{
+    digitalWrite(ce_pin_, LOW);
+}
+
+void Nrf24l01::csn_high()
+{
+    digitalWrite(csn_pin_, HIGH);
+}
+
+void Nrf24l01::csn_low()
+{
+    digitalWrite(csn_pin_, LOW);
+}
+
+uint8_t Nrf24l01::flush_tx()
+{
+    uint8_t st = 0xFF;
+
+    csn_low();
+    st = spi->transfer(FLUSH_TX);
+    csn_high();
+
+    return st;
+}
+
+uint8_t Nrf24l01::flush_rx()
+{
+    uint8_t st = 0xFF;
+
+    csn_low();
+    st = spi->transfer(FLUSH_RX);
+    csn_high();
+
+    return st;
+}
+
+uint8_t Nrf24l01::spi_write_reg(uint8_t addr, uint8_t data)
+{
+    uint8_t st = 0xFF;
+
+    // New cmd must be started by a high to low transition on CSN
+    csn_low();
+    st = spi->transfer(W_REGISTER | addr);
+    spi->transfer(data);
+    csn_high();
+
+    return st;
+}
+
+uint8_t Nrf24l01::spi_read_reg(uint8_t addr, uint8_t *data)
+{
+    uint8_t st = 0xFF;
+
+    csn_low();
+    st = spi->transfer(R_REGISTER | addr);
+    *data = spi->transfer(0xFF);
+    csn_high();
+
+    return st;
+}
+
+uint8_t Nrf24l01::spi_write_buffer(uint8_t addr, uint8_t *buffer, uint8_t bytes)
+{
+    uint8_t st = 0xFF;
+
+    csn_low();
+    st = spi->transfer(W_REGISTER | addr);
+    for (int i = 0; i < bytes; i++)
+    {
+        spi->transfer(buffer[i]);
+    }
+    csn_high();
+
+    return st;
+}
+
+uint8_t Nrf24l01::spi_read_buffer(uint8_t addr, uint8_t *buffer, uint8_t bytes)
+{
+    uint8_t st = 0xFF;
+
+    csn_low();
+    spi->transfer(R_REGISTER | addr);
+    for (int i = 0; i < bytes; i++)
+    {
+        buffer[i] = spi->transfer(0xFF);
+    }
+    csn_high();
+
+    return st;
+}
+
+void Nrf24l01::set_txrx_mode(ModeType mode)
+{
+    uint8_t control_reg = 0;
+    spi_read_reg(CONFIG, &control_reg);
+    if (mode == MODE_TX)
+    {
+        control_reg |= (1 << PRIM_RX);
+    }
+    else
+    {
+        control_reg &= ~(1 << PRIM_RX);
+    }
+    spi_write_reg(CONFIG, control_reg);
+}
+
+void Nrf24l01::set_channel(uint8_t ch)
+{
+    spi_write_reg(RF_CH, ch);
+}
+
+uint8_t Nrf24l01::tx_packet(uint8_t *tx_buf, uint8_t len)
+{
+    uint8_t i;
+    uint8_t status;
+    uint8_t timeout;
+
+    flush_tx();
+    // go to standby-I mode
+    ce_low();
+    // set to tx mode
+    set_txrx_mode(MODE_TX);
+    csn_low();
+    // Write cmd to write payload
+    spi->transfer(W_TX_PAYLOAD);
+    // Write payload
+    for (i = 0; i < len; i++)
+    {
+        spi->transfer(tx_buf[i]);
+    }
+    csn_high();
+
+    // Start transmission
+    ce_high();
+
+    timeout = 3;
+    do
+    {
+        spi_read_reg(STATUS, &status);
+        if (status & MAX_RT)
+        {
+            flush_tx();
+            return STATUS_MAX_RT;
+        }
+        if (status & TX_DS)
+        {
+            return STATUS_OK;
+        }
+    } while (timeout--);
+
+    return STATUS_TIMEOUT;
+}
+
+// data is ready when: 1) RX_DR is asserted, 2) RX_DR is not asserted
+// but RX FIFO is not empty.
+bool Nrf24l01::is_data_ready()
+{
+    uint8_t value;
+
+    spi_read_reg(STATUS, &value);
+    if (value & RX_DR)
+        return true;
+    spi_read_reg(FIFO_STATUS, &value);
+    if (value & RX_EMPTY)
+        return false;
+    return true;
+}
+
+uint8_t Nrf24l01::data_len()
+{
+    uint8_t len;
+    csn_low();
+    len = spi->transfer(R_RX_PL_WID);
+    csn_high();
+    return len;
+}
+
+// read data from the RX FIFO
+uint8_t Nrf24l01::get_data(uint8_t *data)
+{
+    uint8_t len = data_len();
+    // Flush RX FIFO if the read value is larger than 32 bytes
+    if (len > 32)
+    {
+        len = 0;
+        flush_rx();
+        goto ret;
+    }
+
+    csn_low();
+    spi->transfer(R_RX_PAYLOAD);
+    for (int i = 0; i < len; i++)
+        data[i] = spi->transfer(0xFF);
+    csn_high();
+
+ret:
+    // Clear RX_DR flag
+    spi_write_reg(STATUS, RX_DR);
+    return len;
+}
---
+++ nrf24l01.h
@@ -0,0 +1,202 @@
+/*******************************************************************************
+* @file     nrf24l01.h
+* @author   lcc
+* @version  1.0
+* @date     10-Jan-2022
+* @brief    Register Map and methods of NRF24L01
+*******************************************************************************/
+
+#ifndef _NRF24L01_H_
+#define _NRF24L01_H_
+
+#include <Arduino.h>
+#include <SPI.h>
+
+/* Memory Map */
+#define CONFIG      0x00
+#define EN_AA       0x01
+#define EN_RXADDR   0x02
+#define SETUP_AW    0x03
+#define SETUP_RETR  0x04
+#define RF_CH       0x05
+#define RF_SETUP    0x06
+#define STATUS      0x07
+#define OBSERVE_TX  0x08
+#define RPD         0x09
+#define RX_ADDR_P0  0x0A
+#define RX_ADDR_P1  0x0B
+#define RX_ADDR_P2  0x0C
+#define RX_ADDR_P3  0x0D
+#define RX_ADDR_P4  0x0E
+#define RX_ADDR_P5  0x0F
+#define TX_ADDR     0x10
+#define RX_PW_P0    0x11
+#define RX_PW_P1    0x12
+#define RX_PW_P2    0x13
+#define RX_PW_P3    0x14
+#define RX_PW_P4    0x15
+#define RX_PW_P5    0x16
+#define FIFO_STATUS 0x17
+#define DYNPD       0x1C
+#define FEATRUE     0x1D
+
+/* Bit Mnemonics */
+/* CONFIG */
+#define MASK_RX_DR  ((uint8_t)0x40)
+#define MASK_TX_DS  ((uint8_t)0x20)
+#define MASK_MAX_RT ((uint8_t)0x10)
+#define EN_CRC      ((uint8_t)0x08)
+#define CRCO        ((uint8_t)0x04)
+#define PWR_UP      ((uint8_t)0x02)
+#define PRIM_RX     ((uint8_t)0x01)
+/* EN_AA */
+#define ENAA_P5     ((uint8_t)0x20)
+#define ENAA_P4     ((uint8_t)0x10)
+#define ENAA_P3     ((uint8_t)0x08)
+#define ENAA_P2     ((uint8_t)0x04)
+#define ENAA_P1     ((uint8_t)0x02)
+#define ENAA_P0     ((uint8_t)0x01)
+/* EN_RXADDR */
+#define ERX_P5      ((uint8_t)0x20)
+#define ERX_P4      ((uint8_t)0x10)
+#define ERX_P3      ((uint8_t)0x08)
+#define ERX_P2      ((uint8_t)0x04)
+#define ERX_P1      ((uint8_t)0x02)
+#define ERX_P0      ((uint8_t)0x01)
+/* SETUP_AW */
+#define AW          ((uint8_t)0x03)
+#define AW_RERSERVED    ((uint8_t)0x0)
+#define AW_3BYTES       ((uint8_t)0x1)
+#define AW_4BYTES       ((uint8_t)0x2)
+#define AW_5BYTES       ((uint8_t)0x3)
+/* SETUP_RETR */
+#define ARD         ((uint8_t)0xF0)
+#define ARD_250US   ((uint8_t)0x00)
+#define ARD_500US   ((uint8_t)0x10)
+#define ARD_750US   ((uint8_t)0x20)
+#define ARD_1000US  ((uint8_t)0x30)
+#define ARD_2000US  ((uint8_t)0x70)
+#define ARD_4000US  ((uint8_t)0xF0)
+#define ARC         ((uint8_t)0x0F)
+/* RF_CH */
+#define MASK_RF_CH  ((uint8_t)0x7F)
+/* RF_SETUP */
+#define CONT_WAVE   ((uint8_t)0x80)
+#define RF_DR_LOW   ((uint8_t)0x20)
+#define PLL_LOCK    ((uint8_t)0x10)
+#define RF_DR_HIGH  ((uint8_t)0x08)
+#define RF_PWR      ((uint8_t)0x06)
+/* STATUS */
+#define RX_DR       ((uint8_t)0x40)
+#define TX_DS       ((uint8_t)0x20)
+#define MAX_RT      ((uint8_t)0x10)
+#define RX_P_NO     ((uint8_t)0x0E)
+#define TX_FULL     ((uint8_t)0x01)
+/* OBSERVE_TX */
+#define PLOS_CNT    ((uint8_t)0xF0)
+#define ARC_CNT     ((uint8_t)0x0F)
+/* RPD */
+#define MASK_RPD    ((uint8_t)0x01)
+/* FIFO_STATUS */
+#define TX_REUSE    ((uint8_t)0x40)
+#define FIFO_FULL   ((uint8_t)0x20)
+#define TX_EMPTY    ((uint8_t)0x10)
+#define RX_FULL     ((uint8_t)0x02)
+#define RX_EMPTY    ((uint8_t)0x01)
+/* DYNPD */
+#define DPL_P5      ((uint8_t)0x20)
+#define DPL_P4      ((uint8_t)0x10)
+#define DPL_P3      ((uint8_t)0x08)
+#define DPL_P2      ((uint8_t)0x04)
+#define DPL_P1      ((uint8_t)0x02)
+#define DPL_P0      ((uint8_t)0x01)
+/* FEATURE */
+#define EN_DPL      ((uint8_t)0x04)
+#define EN_ACK_PAY  ((uint8_t)0x02)
+#define EN_DYN_ACK  ((uint8_t)0x01)
+
+/* Instruction Mnemonics */
+#define R_REGISTER    0x00
+#define W_REGISTER    0x20
+#define REGISTER_MASK 0x1F
+#define R_RX_PAYLOAD  0x61
+#define W_TX_PAYLOAD  0xA0
+#define FLUSH_TX      0xE1
+#define FLUSH_RX      0xE2
+#define REUSE_TX_PL   0xE3
+#define R_RX_PL_WID   0x60
+#define W_ACK_PAYLOAD 0xA0
+#define W_TX_PAYLOAD_NOACK  0xB0
+#define NOP           0xFF
+
+/* Status */
+#define STATUS_OK       0
+#define STATUS_FAIL     -1
+#define STATUS_MAX_RT   -2
+#define STATUS_TIMEOUT  -3
+
+class Nrf24l01
+{
+public:
+
+    typedef enum
+    {
+        MODE_TX = 0,
+        MODE_RX
+    } ModeType;
+
+    typedef enum
+    {
+        LENGTH_DYN = 0,
+        LENGTH_FIX
+    } LengthMode;
+
+    Nrf24l01(uint8_t ce_pin = 8, uint8_t csn_pin = 7,
+             SPIClass *spi_obj = &SPI);
+
+    void init();
+    void config();
+    bool is_available();
+
+    void ce_low();
+    void ce_high();
+
+    void csn_low();
+    void csn_high();
+
+    void set_txrx_mode(ModeType mode);
+    void set_channel(uint8_t ch);
+    void set_tx_addr(uint8_t *addr, uint8_t len);
+    void set_rx_addr(uint8_t pipe, uint8_t *addr, uint8_t len);
+    uint8_t flush_tx();
+    uint8_t flush_rx();
+
+    uint8_t tx_packet(uint8_t *tx_buf, uint8_t len);
+    bool is_data_ready();
+    uint8_t data_len();
+    uint8_t get_data(uint8_t *data);
+
+    /* for transceiver setting */
+    /* Channel 0 - 127 */
+    uint8_t channel;
+    /* Dynamic Payload Lengt or Fixed Length */
+    LengthMode length_mode;
+    /* The length of the payload can be from 0 to 32 bytes */
+    uint8_t payload_len;
+    /* Auto Retransmit Count */
+    uint8_t repeat_cnt;
+
+private:
+    /* CE Pin */
+    uint8_t ce_pin_;
+    /* CSN */
+    uint8_t csn_pin_;
+    /* SPI obj */
+    SPIClass *spi;
+
+    uint8_t spi_write_reg(uint8_t addr, uint8_t data);
+    uint8_t spi_read_reg(uint8_t addr, uint8_t *data);
+    uint8_t spi_write_buffer(uint8_t addr, uint8_t *buffer, uint8_t bytes);
+    uint8_t spi_read_buffer(uint8_t addr, uint8_t *buffer, uint8_t bytes);
+};
+#endif /* _NRF24L01_H_ */
